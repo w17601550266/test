0b6d46eeeb7441d2bcfc9ea0a57e04ed017d121d
w17601550266 <168156373@qq.com>
2024-12-20 23:35:39 +0800

    test5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { testData } from './common/data'
 import xml2js from 'xml2js'
 
 function App() {
-  const xml=new xml2js.Parser()
-  console.log(xml,'xmlxmlxml')
+  const xml = new xml2js.Parser()
+  console.log(xml, 'xmlxmlxml')
   return (
     <div className="App">
-      {testData.map((item)=><>{item.name}</>)}
+      {testData.map((item) => <>{item.name}</>)}
     </div>
   );
 }
