66f71bd092281096abc8eecf9920d44a4f6eaaa2
w17601550266 <168156373@qq.com>
2024-12-20 23:26:09 +0800

    test3

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,21 @@
 const path = require('path');
 const { override, addWebpackAlias } = require('customize-cra');
 
+const addfallback =(config)=>{
+  config.resolve.fallback={
+    "timers": false,
+    buffer:false,
+  }
+
+  return config
+
+}
 module.exports = override(
   addWebpackAlias({
     '@components': path.resolve(__dirname, 'src/components/'),
     '@utils': path.resolve(__dirname, 'src/utils/'),
     '@src': path.resolve(__dirname, 'src'),
 
-  })
+  }),
+  addfallback
 );
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { testData } from './common/data'
+import xml2js from 'xml2js'
 
 function App() {
+  const xml=new xml2js.Parser()
   return (
     <div className="App">
       {testData.map((item)=><>{item.name}</>)}
