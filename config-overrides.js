b8d49153f53cb77ab08d6a71dc1d8a6468098a10
w17601550266 <168156373@qq.com>
2024-12-20 21:01:10 +0800

    test

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const { override, addWebpackAlias } = require('customize-cra');
+
+module.exports = override(
+  addWebpackAlias({
+    '@components': path.resolve(__dirname, 'src/components/'),
+    '@utils': path.resolve(__dirname, 'src/utils/'),
+  })
+);
