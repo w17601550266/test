769ab7bb0dbcb57a41f96047f6e849bb818a8d7c
w17601550266 <168156373@qq.com>
2024-12-20 21:15:19 +0800

    test2

--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,7 @@ module.exports = override(
   addWebpackAlias({
     '@components': path.resolve(__dirname, 'src/components/'),
     '@utils': path.resolve(__dirname, 'src/utils/'),
+    '@src': path.resolve(__dirname, 'src'),
+
   })
 );
---
+++ src/common/data.js
@@ -0,0 +1,14 @@
+export const testData=[
+    {
+        name:'11',
+        id:1
+    },
+    {
+        name:'22',
+        id:3
+    },
+    {
+        name:'33',
+        id:3
+    },
+]
