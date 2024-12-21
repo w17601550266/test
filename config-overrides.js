8215fe9cdeb98b116ee0ecc2f43e464115101ab2
w17601550266 <168156373@qq.com>
2024-12-21 08:04:47 +0800

    test8

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,46 @@
 const path = require('path');
-const { override, addWebpackAlias, addWebpackPlugin } = require('customize-cra');
-const Polyfill = require('node-polyfill-webpack-plugin')
+// const { override, addWebpackAlias, addWebpackPlugin } = require('customize-cra');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
-const addfallback =(config)=>{
-  config.resolve.fallback={
-    "timers": false,
-    buffer:false,
+// module.exports = override(
+//   addWebpackAlias({
+//     '@components': path.resolve(__dirname, 'src/components/'),
+//     '@utils': path.resolve(__dirname, 'src/utils/'),
+//     '@src': path.resolve(__dirname, 'src'),
+
+//   }),
+//   addWebpackPlugin(new NodePolyfillPlugin())
+//   // addfallback
+// );
+
+// config-overrides.js
+module.exports =
+  (config, env) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        '@src': path.join(__dirname, 'src'),
+        '@common': path.join(__dirname, 'src/common'),
+        '@store': path.join(__dirname, 'src/store')
+      }
+    };
+
+    config.plugins.push(new NodePolyfillPlugin());
+    if (env === 'production') {
+      config.devtool = false;
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        cacheGroups: {
+          reactVendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)/,
+            name: 'vendors-react',
+            chunks: 'all'
+          }
+
+        }
+      };
+    }
+
+    return config;
   }
-
-  return config
-
-}
-module.exports = override(
-  addWebpackAlias({
-    '@components': path.resolve(__dirname, 'src/components/'),
-    '@utils': path.resolve(__dirname, 'src/utils/'),
-    '@src': path.resolve(__dirname, 'src'),
-
-  }),
-  addWebpackPlugin(new Polyfill())
-  // addfallback
-);
