d5f8913b3e78f59bc083074728948f4ae26af06e
w17601550266 <168156373@qq.com>
2024-12-21 10:39:36 +0800

    test41

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { testData,test } from '@common/data'
 
 function App() {
-
+console.log(test(), '11111')
   return (
     <div className="App">
       {testData.map((item) => <>{item.name}</>)}
