59526bfc592b1c55115ae98cbad5c71dd3df714d
w17601550266 <168156373@qq.com>
2024-12-21 09:33:25 +0800

    test25

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { testData } from './common/data'
+import { testData,  } from '@common/data'
 // import xml2js from 'xml2js'
 
+
+
 function App() {
-  // const xml = new xml2js.Parser()
-  // console.log(xml, 'xmlxmlxml')
+
   return (
     <div className="App">
       {testData.map((item) => <>{item.name}</>)}
--- src/common/data.js
+++ src/common/data.js
@@ -1,14 +1,21 @@
-export const testData=[
+import xml2js from 'xml2js'
+export const testData = [
     {
-        name:'11',
-        id:1
+        name: '11',
+        id: 1
     },
     {
-        name:'22',
-        id:3
+        name: '22',
+        id: 3
     },
     {
-        name:'33',
-        id:3
+        name: '33',
+        id: 3
     },
 ]
+
+export const
+    test = () => {
+        const xml = new xml2js.Parser()
+        console.log(xml, 'xmlxmlxml')
+    }
