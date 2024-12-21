6c00158565ff38283d9770e43d7712056e0b5d5c
w17601550266 <168156373@qq.com>
2024-12-21 09:44:23 +0800

    test27

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { testData,test  } from '@common/data'
+import { testData  } from '@common/data'
 
 
 
 function App() {
-console.log(test)
+
   return (
     <div className="App">
       {testData.map((item) => <>{item.name}</>)}
